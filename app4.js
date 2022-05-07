301908addfe2391d91b45677232f54d514c33739
mangkcat <103752647+mangkcat@users.noreply.github.com>
2022-05-07 21:45:49 +0900

    Add files via upload

---
+++ app4.js
@@ -0,0 +1,63 @@
+function sayHello(name0fPerson, age){
+    console.log("Hi my name is" + name0fPerson + "and I'm"  +age);
+};
+ sayHello("a", 1);
+ sayHello("h", 3);
+ sayHello("g", 4);
+ sayHello("j", 4);
+ sayHello("v", 5);
+ sayHello("r", 5);
+
+
+
+ function plus(firstNumber, secondNomber) {
+     console.log(firstNumber + secondNomber);
+ };
+ function divide(a, b){
+     console.log(a / b);
+ };
+ plus(60, 8);
+ divide(98, 20);
+
+
+
+
+ const toBuy = ("potato", "tomato", "pizza");
+
+ console.log(toBuy);
+ toBuy[2] = "water";
+ console.log(toBuy);
+ toBuy.push("meat");
+ console.log(toBuy);
+
+
+
+
+
+
+
+
+
+
+ const plyer = {
+     name: "mangk",
+     age: 1,
+ };
+ console.log(player);
+ player.name = "mangk";
+ console.log(player);
+ player.yey = "soon";
+ console.log(player, console);
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app6.js
@@ -0,0 +1,9 @@
+const age = 96;
+function calculateKrAge(age) {
+    98;
+    return "hello";
+}
+
+const krAge = calculateKrAge(age);
+
+console.log(krAge);
