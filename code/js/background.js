fde2ad747983e94c4591a7158adac7fd0e10f0c5
mangkcat <103752647+mangkcat@users.noreply.github.com>
2022-07-21 00:14:50 +0900

    Add files via upload
    555

---
+++ code/js/background.js
@@ -0,0 +1,9 @@
+const images = ["탬플릿.jpg", "다운로드.jpg", "1.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
